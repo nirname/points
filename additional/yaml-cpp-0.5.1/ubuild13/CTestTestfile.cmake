f925f461c6339368fb721741afd382f21957ac1d
Nikolay Rozhkov <magistrgloom@gmail.com>
2014-03-04 19:22:06 +0400

    Ubuntu 13.10 build

---
+++ additional/yaml-cpp-0.5.1/ubuild13/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/patty/Projects/My/Points/additional/yaml-cpp-0.5.1
+# Build directory: /home/patty/Projects/My/Points/additional/yaml-cpp-0.5.1/ubuild13
+# 
+# This file includes the relevent testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+SUBDIRS(test)
+SUBDIRS(util)
