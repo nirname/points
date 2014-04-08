f836520faed07396d38467c749789150a1b983b8
Nikolay Rozhkov <magistrgloom@gmail.com>
2014-04-08 14:22:38 +0400

    Controls and some fonts

--- source/controls.h
+++ source/controls.h
@@ -2,6 +2,18 @@
 
 namespace controls {
 
+	class Key;
+
+	class Key {
+		unsigned char value;
+		int special;
+
+		//bool operator < (const Key &) const;
+	};
+
+	//bool Key::operator < (const Key & _key) const {
+	//}
+
 	#include "controls/action.h"
 	#include "controls/control_handler.h"
 
