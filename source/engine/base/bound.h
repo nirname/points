5d3e2bb893afb12fff5c9a260e0895fb8e56a670
Nikolay Rozhkov <magistrgloom@gmail.com>
2014-04-11 13:38:10 +0400

    Doxygen

--- source/engine/base/bound.h
+++ source/engine/base/bound.h
@@ -4,16 +4,27 @@ namespace engine {
 
 	struct Bound {
 
-		Point initial, final;
+		/// Left bottom point
+		Point initial;
+		/// Right top point
+		Point final;
 
+		/// Create bound with inital point at (0, 0) and final point from a parameter
 		Bound(const Point & _final);
+		/// Create with initial and final points
 		Bound(Point _initial, Point _final);
+		/// Copy constructor
 		Bound(const Bound & _bound);
 
+		/// Сheck if bound contains a point
 		bool contains(Point x) const;
 
+		/// Shift initial and final points in forward direction
 		Bound & operator += (const Point & x);
+		/// Shift initial and final point in backward direction
 		Bound & operator -= (const Point & x);
+
+		/// Calculates intersection of bounds
 		Bound & operator &= (const Bound & x);
 
 	};
