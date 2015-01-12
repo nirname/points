554cbbf9528e595dc3bcab402c29515a2003d73a
Nikolay Rozhkov <magistrgloom@gmail.com>
2015-01-13 01:38:17 +0400

    Documentation & description

--- source/core/aspect_ratio.hpp
+++ source/core/aspect_ratio.hpp
@@ -1,10 +1,19 @@
 #pragma once
 
+/// Relation between screen sides
+///
 struct AspectRatio {
-	unsigned int width, height;
 
+	/// Long side
+	unsigned int width;
+	/// Short size
+	unsigned int height;
+
+	/// Default constructor
 	AspectRatio();
+	/// Creating aspect ratio by long and short sides
 	AspectRatio(unsigned int _width, unsigned int _height);
 
+	/// Relation between long side and short side
 	float aspect() const;
 };
