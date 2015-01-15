0735ccfd6259701ff19912b9d6ca63867d6120d2
Nikolay Rozhkov <magistrgloom@gmail.com>
2015-01-16 02:40:11 +0400

    Corrections for shape convert

--- source/core/convert/convert_shape.hpp
+++ source/core/convert/convert_shape.hpp
@@ -15,6 +15,7 @@ namespace YAML {
 
 		static bool decode(const Node & node, Shape & shape) {
 			std::cout << "Shape: " << std::ends;
+			std::string shape_name;
 
 			if(shape.basic_shape != NULL) {
 				delete shape.basic_shape;
@@ -22,10 +23,9 @@ namespace YAML {
 			}
 
 			if(shape.basic_shape == NULL) {
-				std::string shape_name;
 				try {
 					if(node.IsScalar()) {
-						std::cout << "scalar" << std::endl;
+						std::cout << "(simple) ";
 						shape_name = node.as<std::string>();
 						if(shape_name == "block") {
 							shape.basic_shape = new ::Block;
@@ -40,32 +40,35 @@ namespace YAML {
 						} else if(shape_name == "triangle") {
 							shape.basic_shape = new ::Triangle;
 						} else {
-							std::cout << "Unknown shape name "
+							std::cout << "unknown shape name "
 								<< "`" << shape_name << "', "
 								<< "default is used" << std::endl;
 							shape.basic_shape = new ::Block;
 						}
+						std::cout << "`" << shape_name << "'" << std::endl;
 					} else if(node.IsSequence() && node.size() >= 1) {
-						std::cout << "sequence" << std::endl;
+						std::cout << "(complex) ";
 						shape_name = node[0].as<std::string>();
 						if(shape_name == "ngon") {
 							if(node.size() != 3) {
-								std::cout << "Wrong number of arguments for ngon (should be 3)" << std::endl;
+								std::cout << "wrong number of arguments for ngon (should be 2)" << std::endl;
 								return false;
 							}
 							int angles = node[1].as<int>();
 							int step_over = node[2].as<int>();
 							shape.basic_shape = new ::NGon(angles, step_over);
+							std::cout << "`" << shape_name << "'" << std::endl;
 						} else {
-							std::cout << "Only ngon has arguments" << std::endl;
+							std::cout << "only ngon has arguments" << std::endl;
 							return false;
 						}
 					} else {
 						return false;
 					}
 				} catch(YAML::TypedBadConversion<std::string> & exception) {
-					std::cout << "Wrong shape name" << std::endl;
-					return false;
+					std::cout << "wrong shape name; ";
+				} catch(YAML::TypedBadConversion<int> & exception) {
+					std::cout << "wrong shape options; ";
 				}
 			}
 
@@ -73,64 +76,13 @@ namespace YAML {
 				std::cout << "ok" << std::endl;
 				return false;
 			} else {
-				std::cout << "shape wasn't created" << std::endl;
+				shape.basic_shape = new ::Block;
+				std::cout << "shape wasn't created, default shape is used" << std::endl;
 				return true;
 			}
 
 		}
 
 	};
-
-	/*template<>
-	struct convert<BasicShape *> {
-
-		static Node encode(const BasicShape * & shape) {
-			Node node;
-			return node;
-		}
-
-		static bool decode(const Node & node, BasicShape * & shape) {
-			std::string shape_name;
-
-			if(node.IsScalar()) {
-				std::cout << "scalar" << std::endl;
-				shape_name = node.as<std::string>();
-				if(shape_name == "block") {
-					shape = new ::Block;
-				} else if(shape_name == "circle") {
-					shape = new ::Circle;
-				} else if(shape_name == "star") {
-					shape = new ::Star;
-				} else if(shape_name == "david") {
-					shape = new ::David;
-				} else if(shape_name == "diamond") {
-					shape = new ::Diamond;
-				} else if(shape_name == "triangle") {
-					shape = new ::Triangle;
-				} else {
-					shape = new ::Block;
-				}
-			} else if(node.IsSequence() && node.size() >= 1) {
-				std::cout << "sequence" << std::endl;
-				shape_name = node[0].as<std::string>();
-				if(shape_name == "ngon") {
-					if(node.size() != 3) {
-						std::cout << "Wrong number of arguments for ngon (should be 3)" << std::endl;
-						return false;
-					}
-					int angles = node[1].as<int>();
-					int step_over = node[2].as<int>();
-					shape = new ::NGon(angles, step_over);
-				} else {
-					std::cout << "Only ngon has arguments" << std::endl;
-					return false;
-				}
-			} else {
-				return false;
-			}
-
-			return true;
-		}
-	};*/
 
 }
